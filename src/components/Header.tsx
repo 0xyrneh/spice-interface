986c8ea7c95c390420c75d3ee55013327ba9b003
r2moon <ryuheimat3@gmail.com>
2023-04-07 01:56:33 +0900

    vault mvp

---
+++ src/components/Header.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { NavOption } from "@/types/common";
+
+const NAV_OPTIONS: NavOption[] = [
+  { href: "/", name: "Vaults" },
+  { href: "/portfolio", name: "Portfolio" },
+  { href: "/prologue-nfts", name: "Prologue NFTs" },
+];
+
+const Header = () => {
+  const router = useRouter();
+  const linkClass = (option: NavOption) => {
+    if (
+      router.pathname === option.href ||
+      router.pathname.startsWith(option.href + "/", 0)
+    ) {
+      return "text-yellow-light text-shadow-yellow-light";
+    }
+    return "";
+  };
+
+  return (
+    <div className="hidden md:flex fixed left-0 top-0 right-0 h-[62px] bg-secondary bg-opacity-45 items-center justify-between px-20 font-SuisseIntl font-semibold">
+      <div className="flex-1 flex items-center gap-10">
+        <Image
+          src="/assets/icons/logo.svg"
+          alt="spice"
+          width={108}
+          height={32}
+        />
+        {NAV_OPTIONS.map((item) => (
+          <Link
+            key={item.name}
+            href={item.href}
+            className={`text-gray text-xs xl:text-sm ${linkClass(item)}`}
+          >
+            {item.name.toUpperCase()}
+          </Link>
+        ))}
+      </div>
+      <div className="rounded border-1 border-gray px-3 py-1 flex items-center gap-3">
+        <Image src="/assets/icons/search.svg" alt="" width={14} height={14} />
+        <input
+          className="bg-transparent text-gray font-SuisseIntl text-xs xl:text-sm outline-0 placeholder:text-gray placeholder:text-opacity-50"
+          placeholder="Search Vaults"
+        />
+      </div>
+      <div className="flex-1 flex justify-end">
+        <button className="text-yellow border-2 border-yellow rounded px-2.5 py-1 bg-yellow bg-opacity-10">
+          <span className="text-xs xl:text-sm text-shadow-yellow">
+            CONNECT WALLET
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,44 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        SuisseIntl: ["Suisse Intl", "sans-serif"],
+        Sanomat: ["Sanomat", "sans-serif"],
+      },
+      colors: {
+        "yellow-light": "#FFE3CA",
+        yellow: "#FDA739",
+        secondary: "#1E1E1E",
+        gray: "#C7C7C7",
+        light: "#FAFAF9",
+        red: "#FF255A",
+        green: "#34D399",
+      },
+      fontSize: {
+        "1.5xl": "22px",
+        "2.5xl": "32px",
+      },
+      spacing: {
+        4.5: "18px",
+        5.5: "22px",
+        3.5: "14px",
+        12.5: "50px",
+        25: "100px",
+      },
+      gap: {
+        15: "60px",
+      },
+      opacity: {
+        45: ".45",
+      },
+      boxShadow: {
+        sm: "inset 0px 4px 70px 30px rgba(0, 0, 0, 0.7)",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+    },
   },
   plugins: [],
 };
