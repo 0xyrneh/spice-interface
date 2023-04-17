f1a209a8ebac163426958a7ed0151a6a26455500
r2moon <ryuheimat3@gmail.com>
2023-04-17 21:38:20 +0900

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { FaDiscord } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
@@ -8,9 +9,17 @@ const VaultFooter = () => {
   return (
     <div className="w-full h-[60px] bg-gray-700 bg-opacity-90 hidden sm:flex items-center justify-between px-8 text-gray-200 text-[10px] lg:text-xs xl:text-base">
       <div className="flex gap-4 items-center">
-        <span className="text-green font-bold text-xs">
-          SPICE MARKET IS OPEN
-        </span>
+        <div className="flex items-center">
+          <Image
+            src="/assets/icons/circle-dot.svg"
+            width={28}
+            height={28}
+            alt=""
+          />
+          <span className="text-green font-bold text-xs">
+            SPICE MARKET IS OPEN
+          </span>
+        </div>
         <Link target="__blank" href="https://docs.spicefi.xyz/">
           <FaFile size={16} />
         </Link>
@@ -24,7 +33,7 @@ const VaultFooter = () => {
           <FaTwitter size={16} />
         </Link>
       </div>
-      <span className="text-gray-200 font-bold text-xs">
+      <span className="hidden md:flex text-gray-200 font-bold text-xs">
         SPICE FINANCE (C) 2023
       </span>
     </div>
