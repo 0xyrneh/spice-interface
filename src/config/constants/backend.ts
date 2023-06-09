44a2fa05d5a6d0c08705e7fdee076d757fba8801
r2moon <ryuheimat3@gmail.com>
2023-06-10 00:00:20 +0900

    loans api integration

--- src/config/constants/backend.ts
+++ src/config/constants/backend.ts
@@ -1,7 +1,11 @@
 const DEFAULT_BACKEND_SERVER_URL = "https://testapi.spicefi.xyz/v1";
+const DEFAULT_V2_BACKEND_SERVER_URL = "https://testapi.spicefi.xyz/v2";
 
 export const BACKEND_SERVER_URL =
   process.env.NEXT_PUBLIC_BACKEND_SERVER_URL || DEFAULT_BACKEND_SERVER_URL;
+export const BACKEND_V2_SERVER_URL =
+  process.env.NEXT_PUBLIC_V2_BACKEND_SERVER_URL ||
+  DEFAULT_V2_BACKEND_SERVER_URL;
 
 // vault API URLS
 export const OFFCHAIN_VAULT_API = `${BACKEND_SERVER_URL}/api/off-chain-vaults`;
@@ -18,3 +22,6 @@ export const COLLECTION_API_BASE = `${BACKEND_SERVER_URL}/api/collections`;
 // reservoir API
 export const RESERVOIR_API_BASE = "https://api.reservoir.tools";
 export const RESERVOIR_API_TOKENS_BASE = `${RESERVOIR_API_BASE}/redirect/tokens`;
+
+// blur API
+export const BLUR_API_BASE = `${BACKEND_V2_SERVER_URL}/api/blur`;
