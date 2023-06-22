a9637b1789a369f9191d7323c9424e41f2b72fae
r2moon <ryuheimat3@gmail.com>
2023-06-22 23:15:19 +0900

    update hover

--- src/pages/blur-pts.tsx
+++ src/pages/blur-pts.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { constants, utils } from "ethers";
 import { useWeb3React } from "@web3-react/core";
+import { useRouter } from "next/router";
 import axios from "axios";
 
 import GameSVG from "@/assets/icons/game.svg";
@@ -9,16 +10,21 @@ import BlurSVG from "@/assets/icons/blur-pts.svg";
 import { Button, Card, Stats } from "@/components/common";
 import { BLUR_API_BASE } from "@/config/constants/backend";
 import { formatBlurChart } from "@/utils/formatter";
+import { useAppSelector } from "@/state/hooks";
 
 export default function Portfolio() {
   const [address, setAddress] = useState<string>();
+  const [error, setError] = useState<string>();
   const [checkedAddress, setCheckedAddress] = useState<string>();
   const [vaultInfo, setVaultInfo] = useState();
   const [userInfo, setUserInfo] = useState();
   const [vaultChartInfo, setVaultChartInfo] = useState<any>();
   const [userChartInfo, setUserChartInfo] = useState<any>();
   const [historicalData, setHistoricalData] = useState<any>();
+  const [earnedPoints, setEarnedPoints] = useState(0);
 
+  const router = useRouter();
+  const { vaults } = useAppSelector((state) => state.vault);
   const { account } = useWeb3React();
 
   const fetchEfficiency = async (addr?: string) => {
@@ -68,8 +74,31 @@ export default function Portfolio() {
     fetchEfficiency();
   }, []);
 
+  useEffect(() => {
+    if (checkedAddress && vaultInfo) {
+      const vaultEfficiency = vaultInfo[2];
+      const userEfficiency = userInfo ? userInfo[2] : 0;
+      const userTvl = userChartInfo ? userChartInfo.tvl : 0;
+
+      let points = (vaultEfficiency - userEfficiency) * userTvl;
+
+      if (points <= 0) {
+        points = vaultInfo[7] - (userInfo ? userInfo[7] : 0);
+      }
+
+      setEarnedPoints(points);
+    } else {
+      setEarnedPoints(0);
+    }
+  }, [vaultInfo, userInfo, checkedAddress, userChartInfo]);
+
   const onCheck = () => {
-    fetchEfficiency(address);
+    if (isValidAddress()) {
+      setError(undefined);
+      fetchEfficiency(address);
+    } else {
+      setError("This address is invalid. Please try again.");
+    }
   };
 
   const isValidAddress = () => {
@@ -77,10 +106,18 @@ export default function Portfolio() {
     return utils.isAddress(address.toLowerCase());
   };
 
+  const goVaultDetails = () => {
+    console.log(vaults);
+    const blurVault = vaults.find((vault) => vault.isBlur);
+    if (blurVault) {
+      router.push(`/vault/${blurVault.address}`);
+    }
+  };
+
   return (
     <div className="relative hidden md:flex tracking-wide w-full h-[calc(100vh-112px)] mt-[80px] px-8 pb-5 gap-5 overflow-hidden">
       <div className="flex flex-col items-center justify-center flex-1">
-        <Card className="max-w-[600px] w-[600px] gap-5">
+        <Card className="max-w-[610px] w-[610px] gap-5">
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-2.5 text-white">
               <GameSVG />
@@ -89,43 +126,45 @@ export default function Portfolio() {
               </h2>
             </div>
           </div>
-          <div className="flex h-9 gap-3">
-            <div className="flex border-1 border-gray-200 rounded flex-1 px-3 text-xs">
-              <input
-                className="flex-1 text-white font-medium placeholder:text-gray-200 placeholder:text-opacity-50"
-                placeholder="Enter wallet address"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-              />
-              {checkedAddress && (
-                <button
-                  className="text-orange-900 text-xs font-medium text-shadow-orange-900-sm underline"
-                  onClick={() => {
-                    setAddress("");
-                    setCheckedAddress(undefined);
-                    setUserInfo(undefined);
-                  }}
-                >
-                  RESET
-                </button>
-              )}
-              {!checkedAddress && account && (
-                <button
-                  className="text-orange-900 text-xs font-medium text-shadow-orange-900-sm underline"
-                  onClick={() => setAddress(account)}
-                >
-                  {account.slice(0, 8)}
-                </button>
-              )}
+          <div className="flex flex-col">
+            <div className="flex h-9 gap-3">
+              <div className="flex border-1 border-gray-200 rounded flex-1 px-3 text-xs">
+                <input
+                  className="flex-1 text-white font-medium placeholder:text-gray-200 placeholder:text-opacity-50"
+                  placeholder="Enter wallet address"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+                {checkedAddress && (
+                  <button
+                    className="text-orange-900 text-xs font-medium text-shadow-orange-900-sm underline hover:text-orange-300"
+                    onClick={() => {
+                      setAddress("");
+                      setCheckedAddress(undefined);
+                      setUserInfo(undefined);
+                    }}
+                  >
+                    RESET
+                  </button>
+                )}
+                {!checkedAddress && account && (
+                  <button
+                    className="text-orange-900 text-xs font-medium text-shadow-orange-900-sm underline  hover:text-orange-300"
+                    onClick={() => setAddress(account)}
+                  >
+                    {account.slice(0, 8)}
+                  </button>
+                )}
+              </div>
+              <Button
+                type="primary"
+                className="w-[100px] h-full"
+                onClick={onCheck}
+              >
+                CHECK
+              </Button>
             </div>
-            <Button
-              className="w-[100px] h-full"
-              type={isValidAddress() ? "primary" : "secondary"}
-              disabled={!isValidAddress()}
-              onClick={onCheck}
-            >
-              CHECK
-            </Button>
+            {error && <span className="mt-px text-red text-xs">{error}</span>}
           </div>
 
           {checkedAddress && (
@@ -197,7 +236,11 @@ export default function Portfolio() {
               <span className="text-base text-orange-900 -ml-1">
                 SP-BLUR VAULT
               </span>
-              <Button className="h-5 w-[84px] text-xs" type="primary">
+              <Button
+                className="h-5 w-[84px] text-xs"
+                type="primary"
+                onClick={goVaultDetails}
+              >
                 SEE VAULT
               </Button>
             </div>
@@ -261,24 +304,37 @@ export default function Portfolio() {
             </div>
           </div>
 
-          {/* <div className="flex">
-            <Search
-              placeholder="Search loans"
-              className="xl:flex-none w-full"
-            />
-          </div>
-          <Table
-            containerClassName="flex-1"
-            className="block h-full"
-            rowInfos={getRowInfos()}
-            items={loans}
-            trStyle="h-10"
-            rowStyle="h-8"
-            defaultSortKey="apy"
-            bodyClass="h-[calc(100%-40px)]"
-            isLoading={isFetching}
-            walletConnectRequired={true}
-          /> */}
+          {checkedAddress && earnedPoints > 0 && (
+            <span className="text-orange-900 text-xs border-1 border-orange-900 rounded text-xs py-2 px-1 text-center tracking-normal">
+              {"0x" + checkedAddress.slice(2, 8).toUpperCase() + " "}
+              would have earned {earnedPoints.toFixed(2)}{" "}
+              <span className="font-bold">MORE BLUR POINTS</span> depositing ETH
+              into the SP-BLUR Vault than farming themselves. Will you continue
+              to fade rational decision making anon?{" "}
+              <span
+                className="underline cursor-pointer font-bold hover:text-orange-300"
+                onClick={goVaultDetails}
+              >
+                DEPOSIT NOW
+              </span>
+            </span>
+          )}
+
+          {checkedAddress && earnedPoints < 0 && (
+            <span className="text-orange-900 text-xs border-1 border-orange-900 rounded text-xs py-2 px-1 text-center tracking-normal">
+              {"0x" + checkedAddress.slice(2, 8).toUpperCase() + " "}
+              is earning {Math.abs(earnedPoints).toFixed(2)}{" "}
+              <span className="font-bold">MORE BLUR POINTS</span> farming
+              themselves vs depositing ETH into the SP-BLUR Vault. Do you want
+              to continue spending the time farming yourself?{" "}
+              <span
+                className="underline cursor-pointer font-bold hover:text-orange-300"
+                onClick={goVaultDetails}
+              >
+                DEPOSIT NOW
+              </span>
+            </span>
+          )}
         </Card>
       </div>
     </div>
