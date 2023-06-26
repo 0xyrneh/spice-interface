f33198e656fa858144016c15e80eff70f5634200
daiki saito <daiki.saito92@gmail.com>
2023-06-26 11:59:22 +0900

    fix: padding issue

--- src/components/vaults/News.tsx
+++ src/components/vaults/News.tsx
@@ -1,6 +1,6 @@
 const News = () => {
   return (
-    <div className="bg-gray-700 bg-opacity-[.95] h-[60px] hidden sm:flex items-center justify-center">
+    <div className="bg-gray-700 bg-opacity-[.95] h-[60px] hidden sm:flex items-center justify-center px-8">
       <span className="text-warm-gray-50 font-medium text-base text-center">
         BLUR VAULT IS LIVE. Deposit ETH to earn real yield and protocol airdrops
         from automated NFT-backed lending strategies on Blend 🏜️🐪
